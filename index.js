6d321036a01ebfc14772037ca49d70dbc7266c43
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-05 07:48:25 +0300

    commit tree 814857940712a6e9d1fda558e355d04a8ad830d6

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Follow along with the examples here
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }  
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
